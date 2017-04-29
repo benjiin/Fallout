ad41726b7b6ec562e38a1341359bb8eda339ae86
Benjamin Rosenkranz <benjamin.rosenkranz@gmail.com>
2017-04-29 22:04:14 +0200

    Räume

--- Fallout/Game.cs
+++ Fallout/Game.cs
@@ -18,6 +18,8 @@ namespace Fallout
         Room[] roomE = new Room[10];
         Room[] roomF = new Room[10];
         Room[] roomG = new Room[10];
+        //Room[][] rooms = new Room[7][];
+
 
         public Game()
         {
@@ -28,23 +30,70 @@ namespace Fallout
              * Spalte 3 4 5 6 = Vault 2
              * Spalte 7 8 9 10 = Vault 3
              */
-            for (int i=0; i<10; i++)
+            for (int i = 0; i < 10; i++)
             {
-                roomA[i] = new Room("A" + (i+1));
-                roomB[i] = new Room("B" + (i+1));
-                roomC[i] = new Room("C" + (i+1));
-                roomD[i] = new Room("D" + (i+1));
-                roomE[i] = new Room("E" + (i+1));
-                roomF[i] = new Room("F" + (i+1));
-                roomG[i] = new Room("G" + (i+1));
+                roomA[i] = new Room("A" + (i + 1));
+                roomB[i] = new Room("B" + (i + 1));
+                roomC[i] = new Room("C" + (i + 1));
+                roomD[i] = new Room("D" + (i + 1));
+                roomE[i] = new Room("E" + (i + 1));
+                roomF[i] = new Room("F" + (i + 1));
+                roomG[i] = new Room("G" + (i + 1));
             }
+            //rooms[0] = new Room[11];
+            //rooms[1] = new Room[11];
+            //rooms[2] = new Room[11];
+            //rooms[3] = new Room[11];
+            //rooms[4] = new Room[11];
+            //rooms[5] = new Room[11];
+            //rooms[6] = new Room[11];
 
             /*
              * Räume verbinden
              */
-             //Vault 1 2 x 3 
+            // Vault 1 
             roomA[0].PathNorth = roomB[0];
             roomA[0].PathEast = roomA[1];
+
+            roomA[1].PathNorth = roomB[1];
+            roomA[1].PathEast = roomA[2];
+            roomA[1].PathWest = roomA[0];
+
+            roomA[2].PathNorth = roomB[2];
+            roomA[2].PathWest = roomA[1];
+
+            roomB[0].PathEast = roomB[1];
+            roomB[0].PathSouth = roomA[0];
+
+            roomB[1].PathSouth = roomA[1];
+            roomB[1].PathWest = roomB[0];
+
+            roomB[2].PathSouth = roomA[2];
+            roomB[2].PathUp = roomG[2];
+            // Vault 2 
+            roomA[3].PathNorth = roomB[3];
+            roomA[3].PathEast = roomB[4];
+
+            roomA[4].PathNorth = roomB[4];
+            roomA[4].PathEast = roomB[5];
+            roomA[4].PathWest = roomB[3];
+
+            roomA[5].PathNorth = roomB[5];
+            roomA[5].PathEast = roomB[6];
+            roomA[5].PathWest = roomB[4];
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         }
 
--- Fallout/Program.cs
+++ Fallout/Program.cs
@@ -14,7 +14,25 @@ namespace Fallout
             Console.WriteLine(game.getRoomName());
             Console.ReadKey();
 
+            //int[][] numbers = new int[4][];
+            //numbers[0] = new int[11];
+            //numbers[1] = new int[11];
+            //numbers[2] = new int[11];
+            //numbers[3] = new int[11];
 
+            //for(int i=0; i<numbers[0].Length; i++)
+            //{
+            //    numbers[0][i] = 1;
+            //}
+
+            //foreach (int item in numbers[0])
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+
+            Console.ReadKey();
         }
     }
 }
